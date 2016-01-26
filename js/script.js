db5ea51f0a16f8fab9ef693875f485dcd1c76f2a
PhiliPdB <phlpdbrn@gmail.com>
2016-01-26 19:39:59 +0100

    Made colors draggable

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var colorElements = document.getElementsByClassName('color');
+var element = null;
+
+var colorElementsSize = colorElements.length;
+for (var i = 0; i < colorElementsSize; i++) {
+	element = colorElements[i];
+
+	element.setAttribute('draggable', 'true');
+
+	element.addEventListener('dragstart', function (e) {
+		e.dataTransfer.effectAllowed = 'copy';
+		e.dataTransfer.setData('text/plain', this.id);
+		e.dataTransfer.setData('text/html', this.outerHTML);
+	});
+};

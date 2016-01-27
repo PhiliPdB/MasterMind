423139732b4323374e3b203591bdc73e19e8f666
PhiliPdB <phlpdbrn@gmail.com>
2016-01-27 20:05:32 +0100

    Can now drop colors into a hole.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 var colorElements = document.getElementsByClassName('color');
+var attemptRows = [].slice.call(document.getElementsByClassName('row')).reverse();
+var attempt = 0;
 var element = null;
+
+window.onload = function() {
+	setDropTargets();
+};
 
 // Make the colors draggable
 var colorElementsSize = colorElements.length;
@@ -10,7 +16,43 @@ for (var i = 0; i < colorElementsSize; i++) {
 
 	element.addEventListener('dragstart', function (e) {
 		e.dataTransfer.effectAllowed = 'copy';
-		e.dataTransfer.setData('text/plain', this.id);
-		e.dataTransfer.setData('text/html', this.outerHTML);
+		e.dataTransfer.setData('id', this.id);
 	});
+}
+
+function setDropTargets() {
+	var holes = attemptRows[attempt].getElementsByClassName('hole');
+
+	var length = holes.length;
+	for (var i = 0; i < length; i++) {
+		var hole = holes[i];
+
+		hole.addEventListener('dragover', function (e) {
+			if (e.preventDefault()) e.preventDefault();
+			this.className = 'hole over';
+			e.dataTransfer.dropEffect = 'copy';
+			return false;
+		});
+
+		// to get IE to work
+		hole.addEventListener('dragenter', function (e) {
+			this.className = 'hole over';
+			return false;
+		});
+
+		hole.addEventListener('dragleave', function () {
+			this.className = 'hole';
+		});
+
+		hole.addEventListener('drop', function (e) {
+			if (e.stopPropagation) e.stopPropagation(); // stops the browser from redirecting...why???
+
+			var el = document.getElementById(e.dataTransfer.getData('id'));
+			el.setAttribute('draggable', 'false');
+			el.style.opacity = '.5';
+
+			// Handle the drop
+			hole.className = 'hole';
+		});
+	}
 }

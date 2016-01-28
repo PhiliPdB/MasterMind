dffdcba15ecdef66e7c21228e1e9f2d7e9e84357
PhiliPdB <phlpdbrn@gmail.com>
2016-01-28 20:00:58 +0100

    Can now validate colors :tada:

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var colorElements = document.getElementsByClassName('color');
 var attemptRows = [].slice.call(document.getElementsByClassName('row')).reverse();
+var currentInput = [null, null, null, null];
 var attempt = 0;
 
 window.onload = function() {
@@ -74,8 +75,39 @@ function setDropTargets() {
 			// Handle the drop
 			this.className = 'hole filled ' + e.dataTransfer.getData('id');
 
-			// TODO: Make hole draggble
+			// TODO: Make hole draggable
 			// TODO: Save color so we can check if it is right
 		});
 	}
+}
+
+function checkColors() {
+	if (currentInput.indexOf(null) > -1) {
+		// Not all holes are filled
+		return;
+	}
+
+	var request = new XMLHttpRequest();
+	var params = 'colors=' + currentInput;
+
+	request.open('POST', 'validateColors.php', true);
+
+	//Send the proper header information along with the request
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.setRequestHeader("Content-length", params.length);
+	request.setRequestHeader("Connection", "close");
+
+	request.onreadystatechange = function() {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				// Handle output
+				handleColorValidation(JSON.parse(request.responseText));
+			}
+		}
+	};
+	request.send(params);
+}
+
+function handleColorValidation(validation) {
+	// TODO
 }
---
+++ validateColors.php
@@ -0,0 +1,42 @@
+<?php 
+session_start();
+
+$colors = [];
+if ($_SESSION['colors'] == null) {
+	while (sizeof($colors) < 4) {
+		$randomNumber = intval(round(rand(0, 7)));
+		$found = false;
+		foreach ($colors as $color) {
+			if ($color == $randomNumber) {
+				$found = true;
+				break;
+			}
+		}
+		if (!$found) {
+			$colors[] = $randomNumber;
+		}
+	}
+	$_SESSION['colors'] = $colors;
+} else {
+	$colors = $_SESSION['colors'];
+}
+
+$currentInput = explode(',', $_POST['colors']);
+$output = array(
+	"white" => 0,
+	"black" => 0
+	);
+
+for ($i=0; $i < 4; $i++) { 
+	// First convert to int
+	$currentInput[$i] = intval($currentInput[$i]);
+	// Check for black
+	if ($currentInput[$i] == $colors[$i]) {
+		$output["black"]++;
+	} else if (in_array($currentInput[$i], $colors)) {
+		$output["white"]++;
+	}
+}
+
+echo json_encode($output);
+ ?>

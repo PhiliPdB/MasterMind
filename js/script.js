3283baba14b1f808528f19e6318adac665354afd
PhiliPdB <phlpdbrn@gmail.com>
2016-02-01 19:16:12 +0100

    Remove unsafe headers

--- js/script.js
+++ js/script.js
@@ -55,8 +55,6 @@ function checkColors() {
 
 	//Send the proper header information along with the request
 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	request.setRequestHeader("Content-length", params.length);
-	request.setRequestHeader("Connection", "close");
 
 	request.onreadystatechange = function() {
 		if (request.readyState === XMLHttpRequest.DONE) {

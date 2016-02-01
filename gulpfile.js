68964288ccdd336c98930470396a9ad40995b5ce
PhiliPdB <phlpdbrn@gmail.com>
2016-02-01 09:14:42 +0100

    Change Gulp pipeline
    Now running php server with browser-sync and runnin phpmyadmin server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var shell = require('gulp-shell')
+var phpConnect = require('gulp-connect-php');
+var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
@@ -12,10 +13,47 @@ var paths = {
 	}
 };
 
-gulp.task('default', ['php', 'scss', 'scss_watch']);
+var liveReloadFiles = [
+	'index.php',
+	'css/**/*.css',
+	'js/**/*.js'
+];
 
-gulp.task('php', shell.task('php -S localhost:8000'));
+gulp.task('default', ['serve', 'scss', 'scss_watch']);
+gulp.task('serve', ['connect', 'browser-sync']);
 
+// server config
+gulp.task('connect', function() {
+	// PHP server (will be proxied)
+	phpConnect.server({
+		base: './',
+		hostname: '127.0.0.1',
+		port: 8000
+	});
+
+	// Another server for phpMyAdmin, since connect-php doesn't support multiple bases
+	phpConnect.server({
+		base: 'phpmyadmin',
+		open: false,
+		hostname: '127.0.0.1',
+		port: 1337
+	});
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		files: liveReloadFiles,
+		proxy: '127.0.0.1:8000',
+    	port: 8080,
+	}, function (err, bs) {
+		if (err)
+			console.log(err);
+		else
+			console.log('BrowserSync is ready.');
+	});
+});
+
+// scss stuff
 gulp.task('scss_watch', function() {
 	gulp.watch(paths.styles.src, ['scss']);
 });
@@ -25,4 +63,7 @@ gulp.task('scss', function() {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest(paths.styles.dest))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });

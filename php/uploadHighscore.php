dbfad721ec5b559dcdcc0697365d8f6f71310ce7
PhiliPdB <phlpdbrn@gmail.com>
2016-02-04 09:14:26 +0100

    Improve cookies

--- php/uploadHighscore.php
+++ php/uploadHighscore.php
@@ -7,7 +7,7 @@ session_start();
 $username = $connection->real_escape_string($_POST['username']);
 $score = calculate_score($_SESSION['attempts'], $_SESSION['black'], $_SESSION['white']);
 
-if (strlen($username) > 0 && strlen($username) <= 20 && $score > 0 && $_COOKIE['score'] < $score) {
+if (strlen($username) > 0 && strlen($username) <= 20 && $score > 0 && intval($_COOKIE['score']) < $score) {
 	// Check if username already exists
 	$username_check = $connection->query("SELECT * FROM `highscores` WHERE `nickname` = '$username'")->fetch_array();
 	if (isset($username_check)) {
@@ -15,8 +15,10 @@ if (strlen($username) > 0 && strlen($username) <= 20 && $score > 0 && $_COOKIE['
 	}
 	// Upload highscore
 	$connection->query("INSERT INTO `highscores` (`nickname`, `score`) VALUES ('$username', '$score')");
-	setcookie("score", $score, time() + 3600 * 7 * 52);
-	setcookie("nickname", $username, time() + 3600 * 7 * 52);
+	setcookie("score", $score, time() + 3600 * 7 * 52, "/");
+	setcookie("nickname", $username, time() + 3600 * 7 * 52, "/");
+	$_COOKIE['score'] = $score;
+	$_COOKIE['nickname'] = $username;
 	echo "success";
 }
 

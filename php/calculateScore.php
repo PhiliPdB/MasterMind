dcf39c965472c690ccf74bc080cca704bfce4716
PhiliPdB <phlpdbrn@gmail.com>
2016-02-04 19:54:04 +0100

    Improve score calculation a bit

--- php/calculateScore.php
+++ php/calculateScore.php
@@ -3,7 +3,7 @@ function calculate_score($attempt, $black, $white) {
 	$output = 0;
 	$attempt = -$attempt + 12;
 	if ($attempt > 8) {
-		$output = intval(round(-150 * sqrt($attempt) + 570));
+		$output = intval(round(-150 * sqrt($attempt) + 575));
 	} else if ($attempt < 0) {
 		$output = -100;
 	} else {

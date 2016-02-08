366b973259cadf402347e4c67f123291eaf30ebf
PhiliPdB <phlpdbrn@gmail.com>
2016-02-08 17:27:19 +0100

    Improve mobile drag & drop

--- js/script.js
+++ js/script.js
@@ -287,24 +287,26 @@ function setDropZone(element) {
 function setupMobileDragDrop(color) {
 	addEvent(color, 'touchstart', function (event) {
 		console.log('touchstart');
-		pointerElement = color.cloneNode(false);
-		pointerElement.style.position = 'absolute';
-		pointerElement.style.width = "40px";
-		pointerElement.style.height = "40px";
-		pointerElement.style.borderRadius = "20px";
-		pointerElement.style.boxShadow = "0 0 10px #212121";
-		pointerElement.style.zIndex = 1000;
-		document.body.appendChild(pointerElement);
-		if (event.targetTouches.length == 1) {
-			var touch = event.targetTouches[0];
-			pointerElement.style.left = touch.pageX - 20 + 'px';
-			pointerElement.style.top = touch.pageY - 20 + 'px';
+		if (!document.body.contains(pointerElement)) {
+			pointerElement = color.cloneNode(false);
+			pointerElement.style.position = 'absolute';
+			pointerElement.style.width = "40px";
+			pointerElement.style.height = "40px";
+			pointerElement.style.borderRadius = "20px";
+			pointerElement.style.boxShadow = "0 0 10px #212121";
+			pointerElement.style.zIndex = 1000;
+			document.body.appendChild(pointerElement);
+			if (event.targetTouches.length === 1) {
+				var touch = event.targetTouches[0];
+				pointerElement.style.left = touch.pageX - 20 + 'px';
+				pointerElement.style.top = touch.pageY - 20 + 'px';
+			}
 		}
 	});
 
 	addEvent(color, 'touchmove', function (event) {
 		console.log('touchmove');
-		if (event.targetTouches.length == 1) {
+		if (event.targetTouches.length === 1) {
 			event.preventDefault();
 			var touch = event.targetTouches[0];
 			pointerElement.style.left = touch.pageX - 20 + 'px';
@@ -314,7 +316,22 @@ function setupMobileDragDrop(color) {
 
 	addEvent(color, 'touchend', function (event) {
 		console.log('touchend');
+		var location = getPosition(pointerElement);
+		location.x += 20;
+		location.y += 20;
 		document.body.removeChild(pointerElement);
+		// Check if is hovering hole
+		var holes = attemptRows[attempt].getElementsByClassName('hole');
+
+		var length = holes.length;
+		for (var i = 0; i < length; i++) {
+			if (holes[i].className.indexOf('little') > -1) continue;
+			position = getPosition(holes[i]);
+			if (location.x >= position.x && location.x <= position.x + 40
+				&& location.y >= position.y && location.y <= position.y + 40) {
+				// TODO handle drop
+			}
+		}
 	});
 }
 
@@ -398,6 +415,18 @@ function getCookie(name) {
 	var value = '; ' + document.cookie;
 	var parts = value.split('; ' + name + '=');
 	if (parts.length == 2) return parts.pop().split(';').shift();
+}
+
+function getPosition(element) {
+	var xPosition = 0;
+	var yPosition = 0;
+	
+	while(element) {
+		xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+		yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+		element = element.offsetParent;
+	}
+	return { x: xPosition, y: yPosition };
 }
 
 // This is a function from https://github.com/remy/html5demos

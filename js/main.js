40bb40c1275a957a61f5439cf3f0814c43f56d0c
PhiliPdB <phlpdbrn@gmail.com>
2016-02-08 18:51:51 +0100

    Rename scripts and move to other files

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var colorElements = document.getElementsByClassName('color');
 var attemptRows = [].slice.call(document.getElementsByClassName('row')).reverse();
 var currentInput = [null, null, null, null];
 var attempt = 0;
-var pointerElement;
 
 window.onload = function() {
 	setupAttemptRow();
@@ -281,57 +280,6 @@ function setDropZone(element) {
 			};
 			e.dataTransfer.setData('Text', JSON.stringify(data));
 		});
-	});
-}
-
-function setupMobileDragDrop(color) {
-	addEvent(color, 'touchstart', function (event) {
-		console.log('touchstart');
-		if (!document.body.contains(pointerElement)) {
-			pointerElement = color.cloneNode(false);
-			pointerElement.style.position = 'absolute';
-			pointerElement.style.width = "40px";
-			pointerElement.style.height = "40px";
-			pointerElement.style.borderRadius = "20px";
-			pointerElement.style.boxShadow = "0 0 10px #212121";
-			pointerElement.style.zIndex = 1000;
-			document.body.appendChild(pointerElement);
-			if (event.targetTouches.length === 1) {
-				var touch = event.targetTouches[0];
-				pointerElement.style.left = touch.pageX - 20 + 'px';
-				pointerElement.style.top = touch.pageY - 20 + 'px';
-			}
-		}
-	});
-
-	addEvent(color, 'touchmove', function (event) {
-		console.log('touchmove');
-		if (event.targetTouches.length === 1) {
-			event.preventDefault();
-			var touch = event.targetTouches[0];
-			pointerElement.style.left = touch.pageX - 20 + 'px';
-			pointerElement.style.top = touch.pageY - 20 + 'px';
-		}
-	});
-
-	addEvent(color, 'touchend', function (event) {
-		console.log('touchend');
-		var location = getPosition(pointerElement);
-		location.x += 20;
-		location.y += 20;
-		document.body.removeChild(pointerElement);
-		// Check if is hovering hole
-		var holes = attemptRows[attempt].getElementsByClassName('hole');
-
-		var length = holes.length;
-		for (var i = 0; i < length; i++) {
-			if (holes[i].className.indexOf('little') > -1) continue;
-			position = getPosition(holes[i]);
-			if (location.x >= position.x && location.x <= position.x + 40
-				&& location.y >= position.y && location.y <= position.y + 40) {
-				// TODO handle drop
-			}
-		}
 	});
 }
 

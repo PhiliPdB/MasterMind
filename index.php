12c2fc01d3f578b002688e0430627168eefb2f0a
PhiliPdB <phlpdbrn@gmail.com>
2016-02-10 18:49:11 +0100

    Improve styling

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('php/resetColors.php');
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	
 	<title>Mastermind</title>
 	

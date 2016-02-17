ff733e45a8cab989d5c1575ca232a6ae6bba44f0
PhiliPdB <phlpdbrn@gmail.com>
2016-02-17 22:52:57 +0100

    Add favicons

---
+++ favicons.php
@@ -0,0 +1,26 @@
+<?php 
+$path = 'favicons/generated';
+ ?>
+<link rel="apple-touch-icon" sizes="57x57" href="<?=$path?>/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?=$path?>/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?=$path?>/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?=$path?>/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?=$path?>/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?=$path?>/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?=$path?>/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?=$path?>/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?=$path?>/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="<?=$path?>/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?=$path?>/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="<?=$path?>/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?=$path?>/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?=$path?>/manifest.json">
+<link rel="mask-icon" href="<?=$path?>/safari-pinned-tab.svg" color="#8b5a2b">
+<link rel="shortcut icon" href="<?=$path?>/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="MasterMind">
+<meta name="application-name" content="MasterMind">
+<meta name="msapplication-TileColor" content="#8b5a2b">
+<meta name="msapplication-TileImage" content="<?=$path?>/mstile-144x144.png">
+<meta name="msapplication-config" content="<?=$path?>/browserconfig.xml">
+<meta name="theme-color" content="#8b5a2b">
+ 

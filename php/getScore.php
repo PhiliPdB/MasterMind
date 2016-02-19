4fd56a9520287ce58d10d28299bba9b23fcd9526
PhiliPdB <phlpdbrn@gmail.com>
2016-02-19 20:05:20 +0100

    Probably fix #9

--- php/getScore.php
+++ php/getScore.php
@@ -1,9 +1,7 @@
 <?php 
 require("calculateScore.php");
 
-session_start([
-	"read_and_close" => true
-]);
+session_start();
 
 echo json_encode(array("score" => calculate_score($_SESSION['attempts'], $_SESSION['black'], $_SESSION['white'])));
  ?>

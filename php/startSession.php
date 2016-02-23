3f62b05e5c844a3ef7deee20045be51980ba1936
PhiliPdB <phlpdbrn@gmail.com>
2016-02-23 18:47:35 +0100

    Improve some session stuff

--- php/startSession.php
+++ php/startSession.php
@@ -1,7 +1,15 @@
 <?php
-$timeout = 3600 * 24;
+$timeout = 3600 * 24 * 2; // Two days
 session_set_cookie_params($timeout);
 ini_set('session.gc_maxlifetime', $timeout);
 
 session_start();
+
+if (!isset($_SESSION['CREATED'])) {
+	$_SESSION['CREATED'] = time();
+} else if (time() - $_SESSION['CREATED'] > 1800) {
+	// session started more than 30 minutes ago
+	session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+	$_SESSION['CREATED'] = time();  // update creation time
+}
  ?>
